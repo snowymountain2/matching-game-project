9bdecee32197c3c90f44bcce35f2c1f427ee195a
snowymountain2 <snowymountain2@snowy.com>
2021-02-28 17:24:20 -0500

    data capturing

--- src/Components/Mode.js
+++ src/Components/Mode.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@material-ui/core';
+import { useState } from 'react';
 
 const Mode = () => {
+
+    const [gameMode, setGameMode] = useState("female")
+
+    const grabGameMode = (e) => {
+        e.preventDefault();
+        console.log(e)
+    }
+
     return (
         <div>
             <FormControl component="fieldset">
                 <FormLabel component="legend">Mode</FormLabel>
-                <RadioGroup row aria-label="gender" name="gender1" value={3} onChange={console.log('heee')}>
+                <RadioGroup row aria-label="gender" name="gender1" value={3} onChange={grabGameMode}>
                     <FormControlLabel value="female" control={<Radio />} label="Easy" />
                     <FormControlLabel value="male" control={<Radio />} label="Medium" />
                     <FormControlLabel value="other" control={<Radio />} label="Hard" />
